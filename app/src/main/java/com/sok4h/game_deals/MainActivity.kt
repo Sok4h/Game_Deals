1fc07ec27f048ce047579ab66ecb3480ffcd2922
Camilo Jimenez Florez <camilo.jimenezf@perficient.com>
2023-03-28 09:33:53 -0500

    Initial commit

---
+++ app/src/main/java/com/sok4h/game_deals/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.sok4h.game_deals
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sok4h.game_deals.ui.theme.Game_DealsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Game_DealsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Game_DealsTheme {
+        Greeting("Android")
+    }
+}
